ff038d4cc9d37c3356c1b3f41d55701f5f725362
hackanonimous <esi.segurity@gmail.com>
2024-09-22 18:45:07 -0500

    feet:ultimas funcionalidades de todo app

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import flet as ft
 
 
 class Task(ft.Column):
-    def __init__(self, task_name, task_delete):
+    def __init__(self, task_name, task_status_change, task_delete):
         super().__init__()
+        self.completed = False
         self.task_name = task_name
+        self.task_status_change = task_status_change
         self.task_delete = task_delete
-        self.display_task = ft.Checkbox(value=False, label=self.task_name)
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
         self.edit_name = ft.TextField(expand=1)
 
         self.display_view = ft.Row(
@@ -19,18 +23,19 @@ class Task(ft.Column):
                     controls=[
                         ft.IconButton(
                             icon=ft.icons.CREATE_OUTLINED,
-                            tooltip="Editar",
+                            tooltip="Edit To-Do",
                             on_click=self.edit_clicked,
                         ),
                         ft.IconButton(
-                            icon=ft.icons.DELETE_OUTLINE,
-                            tooltip="Eliminar",
+                            ft.icons.DELETE_OUTLINE,
+                            tooltip="Delete To-Do",
                             on_click=self.delete_clicked,
                         ),
                     ],
                 ),
             ],
         )
+
         self.edit_view = ft.Row(
             visible=False,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
@@ -40,7 +45,7 @@ class Task(ft.Column):
                 ft.IconButton(
                     icon=ft.icons.DONE_OUTLINE_OUTLINED,
                     icon_color=ft.colors.GREEN,
-                    tooltip="Actualizar",
+                    tooltip="Update To-Do",
                     on_click=self.save_clicked,
                 ),
             ],
@@ -59,46 +64,109 @@ class Task(ft.Column):
         self.edit_view.visible = False
         self.update()
 
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change(self)
+
     def delete_clicked(self, e):
         self.task_delete(self)
 
 
 class TodoApp(ft.Column):
+    # application's root control is a Column containing all other controls
     def __init__(self):
         super().__init__()
-        self.nueva_tarea = ft.TextField(hint_text="Escribe tu tarea", expand=True)
-        self.vista_tareas = ft.Column()
-        self.width = 400
+        self.new_task = ft.TextField(
+            hint_text="What needs to be done?", on_submit=self.add_clicked, expand=True
+        )
+        self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            scrollable=False,
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="active"), ft.Tab(text="completed")],
+        )
+
+        self.items_left = ft.Text("0 items left")
+
+        self.width = 600
         self.controls = [
             ft.Row(
+                [ft.Text(value="Todos", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM)],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            ft.Row(
                 controls=[
-                    self.nueva_tarea,
+                    self.new_task,
                     ft.FloatingActionButton(
                         icon=ft.icons.ADD, on_click=self.add_clicked
                     ),
-                ]
+                ],
             ),
-            self.vista_tareas,
+            ft.Column(
+                spacing=25,
+                controls=[
+                    self.filter,
+                    self.tasks,
+                    ft.Row(
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                        controls=[
+                            self.items_left,
+                            ft.OutlinedButton(
+                                text="Clear completed", on_click=self.clear_clicked
+                            ),
+                        ],
+                    ),
+                ],
+            ),
         ]
 
     def add_clicked(self, e):
-        task = Task(self.nueva_tarea.value, self.task_delete)
-        self.vista_tareas.controls.append(task)
-        self.nueva_tarea.value = ""
+        if self.new_task.value:
+            task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+            self.tasks.controls.append(task)
+            self.new_task.value = ""
+            self.new_task.focus()
+            self.update()
+
+    def task_status_change(self, task):
         self.update()
 
     def task_delete(self, task):
-        self.vista_tareas.controls.remove(task)
+        self.tasks.controls.remove(task)
         self.update()
+
+    def tabs_changed(self, e):
+        self.update()
+
+    def clear_clicked(self, e):
+        for task in self.tasks.controls[:]:
+            if task.completed:
+                self.task_delete(task)
+
+    def before_update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        count = 0
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+            )
+            if not task.completed:
+                count += 1
+        self.items_left.value = f"{count} active item(s) left"
 
 
 def main(page: ft.Page):
-    page.title = "Aplicacion de Tareas"
+    page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    # page.update()
-    todo = TodoApp()
+    page.scroll = ft.ScrollMode.ADAPTIVE
 
-    page.add(todo)
+    # create app control and add it to the page
+    page.add(TodoApp())
 
 
 ft.app(main)
